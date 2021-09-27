d72a46c12e657f72581cb87cbdb22eb515c24b52
Evgeni Pandurski <epandurski@gmail.com>
2021-09-27 20:38:10 +0300

    Add LoginScreen.svelte (unfinished)

---
+++ src/components/LoginScreen.svelte
@@ -0,0 +1,94 @@
+<script lang="ts">
+  import Button, { Group, GroupItem, Label, Icon } from '@smui/button'
+  import Menu from '@smui/menu'
+  import List, { Item, Text } from '@smui/list'
+  import Paper, { Title, Content } from '@smui/paper'
+
+  export const snackbarBottom: string = '84px'
+
+  function login(disablePin: boolean): void {
+    console.log(disablePin)
+  }
+
+  let menu: any
+  let resetPin = false
+</script>
+
+<style>
+  .floating, .logo-container, .paper-container {
+    display: flex;
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    justify-content: center;
+  }
+  .logo-container {
+    height: 250px;
+    z-index: 1;
+  }
+  .logo-container :global(img) {
+    height: 100%;
+  }
+  .floating {
+    z-index: 3;
+  }
+  .paper-container {
+    z-index: 2;
+    height: 100%;
+  }
+  .paper-height-limiter {
+    height: auto;
+  }
+  .button-container {
+    margin: 16px 16px;
+  }
+</style>
+
+<div class="paper-container">
+  <div class="paper-height-limiter">
+    <Paper style="margin: 36px 18px; max-width: 600px" elevation={8}>
+      <Title>Welcome to Swaptacular!</Title>
+      <Content>
+        Swaptacular tries to make issuing and holding digital
+        currencies possible for everyone. Currency holders are also
+        called "creditors". This app connects you to a Swaptacular
+        Creditors Agent, which will create and manage your digital
+        wallet for you.
+      </Content>
+    </Paper>
+  </div>
+</div>
+
+<div class="logo-container">
+  <img src="favicon.svg" alt="Logo">
+</div>
+
+<div class="floating">
+  <div class="button-container">
+    <Group variant="raised">
+      <Button on:click={() => login(resetPin)} variant="raised">
+        <Label>{resetPin ? "Reset PIN" : "Login"}</Label>
+      </Button>
+      <div use:GroupItem>
+        <Button
+          on:click={() => menu.setOpen(true)}
+          variant="raised"
+          style="padding: 0; min-width: 36px;"
+          >
+          <Icon class="material-icons" style="margin: 0;">arrow_drop_down</Icon>
+        </Button>
+        <Menu bind:this={menu} anchorCorner="TOP_LEFT">
+          <List>
+            <Item on:SMUI:action={() => resetPin = false}>
+              <Text>Login</Text>
+            </Item>
+            <Item on:SMUI:action={() => resetPin = true}>
+              <Text>Reset&nbsp;PIN</Text>
+            </Item>
+          </List>
+        </Menu>
+      </div>
+    </Group>
+  </div>
+</div>
