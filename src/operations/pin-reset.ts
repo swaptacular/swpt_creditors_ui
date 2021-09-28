eca40a8b19ac27f7d2375dfe1610d6005017b386
Evgeni Pandurski <epandurski@gmail.com>
2021-09-28 22:32:06 +0300

    Add unfinished operations/pin-reset.ts

---
+++ src/operations/pin-reset.ts
@@ -0,0 +1,68 @@
+import { ServerSession, Oauth2TokenSource, HttpResponse, HttpError, ServerSessionError } from '../web-api'
+import type { Wallet, PinInfo } from '../web-api-schemas'
+
+class PinResetContext {
+  constructor(public server: ServerSession, public pinInfoUri: string) { }
+
+  async resetPin(newPin: string): Promise<void> {
+    try {
+      let attemptsLeft = 10
+      while (true) {
+        const pinInfo = await this.fetchPinInfo()
+        const requestBody = {
+          type: 'PinInfo',
+          status: 'on',
+          latestUpdateId: pinInfo.latestUpdateId + 1n,
+          newPin,
+        }
+        try {
+          await this.server.patch(this.pinInfoUri, requestBody, { attemptLogin: true })
+        } catch (e: unknown) {
+          if (e instanceof HttpError && e.status === 409 && attemptsLeft--) continue
+          else throw e
+        }
+        break
+      }
+
+    } catch (e: unknown) {
+      if (e instanceof HttpError) throw new ServerSessionError(`unexpected status code (${e.status})`)
+      else throw e
+    }
+  }
+
+  async logout(): Promise<never> {
+    return await this.server.logout()
+  }
+
+  private async fetchPinInfo(): Promise<PinInfo> {
+    const response = await this.server.get(this.pinInfoUri, { attemptLogin: true }) as HttpResponse<PinInfo>
+    return response.data
+  }
+}
+
+/* If the user is logged in and PIN is not required for potentially
+ * dangerous operations, returns a PIN reset context
+ * instance. Otherwise, returns `undefined`. The obtained PIN reset
+ * context instance can only be used to reset the user's PIN, and
+ * logout. */
+export async function obtainPinResetContext(server?: ServerSession): Promise<PinResetContext | undefined> {
+  server ??= new ServerSession({
+    tokenSource: new Oauth2TokenSource(true),
+    onLoginAttempt: async (login) => {
+      if (confirm('This operation requires authentication. You will be redirected to the login page.')) {
+        return await login()
+      }
+      return false
+    }
+  })
+  const entrypoint = await server.entrypointPromise
+  if (entrypoint === undefined) {
+    return undefined  // not logged in
+  }
+  const walletResponse = await server.getEntrypointResponse() as HttpResponse<Wallet>
+  if (walletResponse.data.requirePin) {
+    return undefined  // PIN is required for potentially dangerous operations
+  }
+  const pinInfoUri = walletResponse.buildUri(walletResponse.data.pinInfo.uri)
+  return new PinResetContext(server, pinInfoUri)
+}
