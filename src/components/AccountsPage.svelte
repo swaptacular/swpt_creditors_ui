063e0a323fd40f8b2d58627ccf1b75a935155588
Evgeni Pandurski <epandurski@gmail.com>
2022-03-13 18:12:20 +0200

    Implement filter in AccountsPage.svelte

--- src/components/AccountsPage.svelte
+++ src/components/AccountsPage.svelte
@@ -15,40 +15,18 @@
   export const snackbarBottom: string = '84px'
   export const scrollElement = document.documentElement
 
+  type AccountData = {
+    uri: string,
+    title: string,
+    confirmed: boolean,
+  }
+
   let currentModel: AccountsModel
+  let scanCoinDialog: boolean
+  let pendingFilterChange: boolean
+  let allAccounts: AccountData[]
   let searchText: string
-  let scanCoinDialog = false
-  let accounts = [
-    {uri: '1', title: 'Evgeni Pandurski', confirmed: true},
-    {uri: '2', title: 'United States Dollar', confirmed: true},
-    {uri: '3', title: 'United States Dollar', confirmed: true},
-    {uri: '', title: 'United States Dollar', confirmed: true},
-    {uri: '', title: 'United States Dollar', confirmed: true},
-    {uri: '', title: 'United States Dollar', confirmed: false},
-    {uri: '', title: 'United States Dollar', confirmed: true},
-    {uri: '', title: 'United States Dollar', confirmed: false},
-    {uri: '', title: 'United States Dollar', confirmed: false},
-    {uri: '', title: 'United States Dollar', confirmed: false},
-    {uri: '', title: 'United States Dollar', confirmed: false},
-    {uri: '', title: 'United States Dollar', confirmed: false},
-    {uri: '', title: 'United States Dollar', confirmed: false},
-    {uri: '', title: 'United States Dollar', confirmed: false},
-    {uri: '', title: 'United States Dollar', confirmed: false},
-    {uri: '', title: 'United States Dollar', confirmed: false},
-    {uri: '', title: 'United States Dollar', confirmed: false},
-    {uri: '', title: 'United States Dollar', confirmed: false},
-    {uri: '', title: 'United States Dollar', confirmed: false},
-    {uri: '', title: 'United States Dollar', confirmed: false},
-    {uri: '', title: 'United States Dollar', confirmed: false},
-    {uri: '', title: 'United States Dollar', confirmed: false},
-    {uri: '', title: 'United States Dollar', confirmed: false},
-    {uri: '', title: 'United States Dollar', confirmed: false},
-    {uri: '', title: 'United States Dollar', confirmed: false},
-    {uri: '', title: 'United States Dollar', confirmed: false},
-    {uri: '', title: 'United States Dollar', confirmed: false},
-    {uri: '', title: 'United States Dollar', confirmed: false},
-    {uri: '', title: 'United States Dollar', confirmed: false},
-  ]
+  let filter: string
 
   function showAccount(accountUri: string): void {
     const scrollTop = scrollElement.scrollTop
@@ -58,26 +36,82 @@
     })
   }
 
-  function resetScroll() {
+  function resetScroll(scrollTop: number = 0, scrollLeft: number = 0) {
     if (scrollElement) {
-      scrollElement.scrollTop = model.scrollTop ?? 0
-      scrollElement.scrollLeft = model.scrollLeft ?? 0
+      scrollElement.scrollTop = scrollTop
+      scrollElement.scrollLeft = scrollLeft
     }
   }
 
   onMount(() => {
-    resetScroll()
+    resetScroll(model.scrollTop, model.scrollLeft)
   })
+
+  function applyFilter(accounts: AccountData[], filter: string): AccountData[] {
+    const words = filter.split(/\s+/u).filter(word => word.length > 0)
+    const regExps = words.map(word => new RegExp(`${word}`, 'ui'))
+    return accounts.filter(account => regExps.every(re => re.test(account.title)))
+  }
+
+  function clearFilter() {
+    resetScroll()
+    filter = searchText = ''
+  }
+
+  function triggerFilterChange() {
+    if (!(filter === searchText || pendingFilterChange)) {
+      pendingFilterChange = true
+      setTimeout(() => {
+        resetScroll()
+        filter = searchText
+        pendingFilterChange = false
+      }, 1000)
+    }
+  }
 
   // TODO: add real implementation
   app
-  model
 
   $: if (currentModel !== model) {
     currentModel = model
-    searchText = model.searchText ?? ''
-    resetScroll()
+    scanCoinDialog = false
+    pendingFilterChange = false
+    allAccounts = [
+      {uri: '1', title: 'Evgeni Pandurski', confirmed: true},
+      {uri: '2', title: 'United States Dollar', confirmed: true},
+      {uri: '3', title: 'United States Dollar', confirmed: true},
+      {uri: '', title: 'United States Dollar', confirmed: true},
+      {uri: '', title: 'United States Dollar', confirmed: true},
+      {uri: '', title: 'United States Dollar', confirmed: false},
+      {uri: '', title: 'United States Dollar', confirmed: true},
+      {uri: '', title: 'United States Dollar', confirmed: false},
+      {uri: '', title: 'United States Dollar', confirmed: false},
+      {uri: '', title: 'United States Dollar', confirmed: false},
+      {uri: '', title: 'United States Dollar', confirmed: false},
+      {uri: '', title: 'United States Dollar', confirmed: false},
+      {uri: '', title: 'United States Dollar', confirmed: false},
+      {uri: '', title: 'United States Dollar', confirmed: false},
+      {uri: '', title: 'United States Dollar', confirmed: false},
+      {uri: '', title: 'United States Dollar', confirmed: false},
+      {uri: '', title: 'United States Dollar', confirmed: false},
+      {uri: '', title: 'United States Dollar', confirmed: false},
+      {uri: '', title: 'United States Dollar', confirmed: false},
+      {uri: '', title: 'United States Dollar', confirmed: false},
+      {uri: '', title: 'United States Dollar', confirmed: false},
+      {uri: '', title: 'United States Dollar', confirmed: false},
+      {uri: '', title: 'United States Dollar', confirmed: false},
+      {uri: '', title: 'United States Dollar', confirmed: false},
+      {uri: '', title: 'United States Dollar', confirmed: false},
+      {uri: '', title: 'United States Dollar', confirmed: false},
+      {uri: '', title: 'United States Dollar', confirmed: false},
+      {uri: '', title: 'United States Dollar', confirmed: false},
+      {uri: '', title: 'United States Dollar', confirmed: false},
+    ]
+    filter = searchText = model.searchText ?? ''
+    resetScroll(model.scrollTop, model.scrollLeft)
   }
+  $: hasAccounts = allAccounts.length > 0
+  $: shownAccounts = applyFilter(allAccounts, filter)
 </script>
 
 <style>
@@ -122,7 +156,7 @@
 
 <Page title="Accounts">
   <svelte:fragment slot="app-bar">
-    {#if accounts.length > 0 }
+    {#if hasAccounts }
       <Row style="height: 83px">
         <div class="search-box">
           <div style="padding-left: 16px; flex-grow: 1" >
@@ -131,12 +165,15 @@
               type="text"
               style="width: 100%"
               label="Filter by name"
+              input$spellcheck="false"
               bind:value={searchText}
+              on:input={triggerFilterChange}
+              on:change={triggerFilterChange}
               >
             </Textfield>
           </div>
           <div style="padding: 4px; flex-grow: 0" >
-            <IconButton class="material-icons" on:click={() => searchText = ''}>backspace</IconButton>
+            <IconButton class="material-icons" on:click={clearFilter}>backspace</IconButton>
           </div>
         </div>
       </Row>
@@ -144,20 +181,27 @@
   </svelte:fragment>
 
   <svelte:fragment slot="content">
-    {#if accounts.length > 0 }
+    {#if hasAccounts}
       <div class="empty-space"></div>
-      <LayoutGrid style="word-break: break-word">
-        {#each accounts as account }
-          <Cell>
-            <Card>
-              <PrimaryAction padded on:click={() => showAccount(account.uri)}>
-                <p class="name" class:confirmed={account.confirmed}>{account.title}</p>
-                <p class="amount">137.00 USD</p>
-              </PrimaryAction>
-            </Card>
-          </Cell>
-        {/each}
-      </LayoutGrid>
+      {#if shownAccounts.length > 0 }
+        <LayoutGrid style="word-break: break-word">
+          {#each shownAccounts as account }
+            <Cell>
+              <Card>
+                <PrimaryAction padded on:click={() => showAccount(account.uri)}>
+                  <p class="name" class:confirmed={account.confirmed}>{account.title}</p>
+                  <p class="amount">137.00 USD</p>
+                </PrimaryAction>
+              </Card>
+            </Cell>
+          {/each}
+        </LayoutGrid>
+      {:else}
+        <p class="no-accounts">
+          There are no accounts matching the specified filter.
+        </p>
+      {/if}
+
     {:else}
       <p class="no-accounts">
         Press
